e197ebc7c41da4d0ed8cec5dc4be7f558d26eff9
Your Name <you@example.com>
2023-07-24 17:21:23 +0500

    Initial commit

---
+++ src/test/java/com/example/sofiyainventoryservice/SofiyaInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sofiyainventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofiyaInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
