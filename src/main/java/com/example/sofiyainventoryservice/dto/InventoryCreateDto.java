07f6caad025ec5c2873be2ad1c2a25d27268b78d
Msi Laptop <hh@gg.com>
2023-07-25 10:46:16 +0500

    created BaseEntity, InventoryEntity, InventoryCreateDto

---
+++ src/main/java/com/example/sofiyainventoryservice/dto/InventoryCreateDto.java
@@ -0,0 +1,19 @@
+package com.example.sofiyainventoryservice.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+import java.util.UUID;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class InventoryCreateDto {
+    @NotBlank(message = "product id not entered")
+    private UUID productId;
+
+    @NotBlank(message = "Product count not entered")
+    private Integer productCount;
+}
--- src/main/java/com/example/sofiyainventoryservice/entity/BaseEntity.java
+++ src/main/java/com/example/sofiyainventoryservice/entity/BaseEntity.java
@@ -9,12 +9,14 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+
 import java.time.LocalDateTime;
 import java.util.UUID;
-
 @MappedSuperclass
 @Getter
 @Setter
+
+
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
