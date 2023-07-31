b427fa72bd81d5db705287791abfb3920b7f84d6
Mr.Coder <elmurodov1106@gmail.com>
2023-07-31 16:27:25 +0500

    project 100% hatolari to'g'irlandi

--- src/main/java/com/example/sofiyainventoryservice/repository/InventoryRepository.java
+++ src/main/java/com/example/sofiyainventoryservice/repository/InventoryRepository.java
@@ -3,10 +3,11 @@ package com.example.sofiyainventoryservice.repository;
 import com.example.sofiyainventoryservice.entity.InventoryEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
-
+@Repository
 public interface InventoryRepository extends JpaRepository<InventoryEntity, UUID> {
 //    List<InventoryEntity> searchInventoryEntitiesByProductIdContainingIgnoreCase(UUID productId, Pageable pageable);
 }
